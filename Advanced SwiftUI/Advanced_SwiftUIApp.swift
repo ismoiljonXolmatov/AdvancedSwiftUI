e228d567ee919bd7473ad75a535b99ddacf9f664
Ismoiljon Xolmatov <ismoiljonxolmatov637@gmail.com>
2024-04-27 13:52:07 +0500

    Initial Commit

---
+++ Advanced SwiftUI/Advanced_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_SwiftUIApp.swift
+//  Advanced SwiftUI
+//
+//  Created by Ismoiljon Xolmatov on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
